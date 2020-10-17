65e2b938143df0c174ed1c6206880db0db78c03e
Romain BEN TALEB <r.ben-taleb@tbs-education.org>
2020-10-17 17:55:44 +0200

    update 17h55 testsunitaires

---
+++ test_grille.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+
+class TestGrille(TestCase):
+    def test_set_up_lettres(self):
+        self.failUnless(True)
+
+    def test_init_grille(self):
+        self.failUnless(True)
+
+    def test_demande_coord(self):
+        self.failUnless(True)
+
+    def test_etat_case_grille(self):
+        self.failUnless(True)
+
+    def test_affiche_grille(self):
+        self.failUnless(True)
+
+    def test_jouer_coup(self):
+        self.failUnless(True)
