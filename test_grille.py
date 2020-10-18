9ea55e3074b70e1221a45ef22f622422041cd50f
Romain BEN TALEB <r.ben-taleb@tbs-education.org>
2020-10-19 01:56:56 +0200

    Commit fort test git

--- test_grille.py
+++ test_grille.py
@@ -19,3 +19,21 @@ class TestGrille(TestCase):
 
     def test_jouer_coup(self):
         self.failUnless(True)
+
+    def test_set_up_lettres(self):
+        self.fail()
+
+    def test_init_grille(self):
+        self.fail()
+
+    def test_demande_coord(self):
+        self.fail()
+
+    def test_etat_case_grille(self):
+        self.fail()
+
+    def test_affiche_grille(self):
+        self.fail()
+
+    def test_jouer_coup(self):
+        self.fail()
